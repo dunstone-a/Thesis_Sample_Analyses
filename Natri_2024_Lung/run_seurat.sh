c6b05009da521c62a42898f9e4edee8d6a46b735
Millie <53158915+dunstone-a@users.noreply.github.com>
2024-12-16 17:27:24 +1100

    Remove script no longer needed

--- Natri_2024_Lung/run_seurat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=lung_sc               # Job name
-#SBATCH --ntasks=1                       # Number of tasks (processes)
-#SBATCH --cpus-per-task=1                # Number of CPU cores per task
-#SBATCH --mem=64G                        # Memory per node
-#SBATCH --time=24:00:00                  # Time limit 
-
-# Run your R script
-R -e 'source("explore_seurat.R")'
